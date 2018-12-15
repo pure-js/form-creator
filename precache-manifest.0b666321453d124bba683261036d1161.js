39d79ab7e1c3b5f7709e2f3b57159398dfc87404
pure-js <3690368+pure-js@users.noreply.github.com>
2018-12-15 23:38:50 +0300

    Updates

---
+++ precache-manifest.0b666321453d124bba683261036d1161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "756ba8814b4e26a30098",
+    "url": "/form-creator/static/js/runtime~main.756ba881.js"
+  },
+  {
+    "revision": "c39366ce0a3b7ca36576",
+    "url": "/form-creator/static/js/main.c39366ce.chunk.js"
+  },
+  {
+    "revision": "9aff4b28fc98ea6b662c",
+    "url": "/form-creator/static/js/1.9aff4b28.chunk.js"
+  },
+  {
+    "revision": "c39366ce0a3b7ca36576",
+    "url": "/form-creator/static/css/main.f3dda8e5.chunk.css"
+  },
+  {
+    "revision": "4f5fa6b86f095e4972ea5792ad0c0709",
+    "url": "/form-creator/index.html"
+  }
+];
